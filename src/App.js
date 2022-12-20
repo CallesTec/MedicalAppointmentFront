27c5c098e6854db22bd783bcb9dde76a936413a9
CallesTec <92549697+CallesTec@users.noreply.github.com>
2022-12-20 10:50:22 -0600

    CliDrHours ok

--- src/App.js
+++ src/App.js
@@ -1,35 +1,51 @@
-
 import './App.css';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 //EC: Import own componets
 import ShowPatients from './components/ShowPatients';
 import CreatePatient from './components/CreatePatient';
 import EditPatient from './components/EditPatient';
-
-import ShowClinics from './components/ClinicComponents/ShowClinics';
-import CreateClinic from './components/ClinicComponents/CreateClinic';
-import EditClinic from './components/ClinicComponents/EditClinic';
-
-import ShowDoctors from './components/DoctorComponents/ShowDoctors'
-import CreateDoctor from './components/DoctorComponents/CreateDoctor';
-import EditDoctor from './components/DoctorComponents/EditDoctor';
-
-import ShowAppointment from './components/AppointmentComponents/ShowAppointment'
-import CreateAppointment from './components/AppointmentComponents/CreateAppointment';
-import EditAppointment from './components/AppointmentComponents/EditAppointment';
-
-
+import ShowClinics from './components/ShowClinics';
+import CreateClinic from './components/CreateClinic';
+import EditClinic from './components/EditClinic';
+import ShowDoctors from './components/ShowDoctors';
+import CreateDoctor from './components/CreateDoctor';
+import EditDoctor from './components/EditDoctor';
+import ShowAppointment from './components/ShowAppointment';
+import CreateAppointment from './components/CreateAppointment';
+import EditAppointment from './components/EditAppointment';
+import ShowHours from './components/HourComponents/ShowHours';
+import CreateHour from './components/HourComponents/CreateHour';
+import EditHour from './components/HourComponents/EditHour';
 import Home from './components/Home';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter> 
         <Routes>
-          <Route path='/' element={ <ShowPatients/> } />
+          <Route path='/' element={ <Home/>} />
+          <Route path='/show' element={ <ShowPatients/>} />
           <Route path='/create' element={ <CreatePatient/>}/>
           <Route path='/edit/:id' element={ <EditPatient/>}/>
+
+          <Route path='/showClinic' element={ <ShowClinics/>} />
+          <Route path='/createClinic' element={ <CreateClinic/>}/>
+          <Route path='/editClinic/:id' element={ <EditClinic/>}/>
+
+          <Route path='/showDoctors' element={ <ShowDoctors/>} />
+          <Route path='/createDoctor' element={ <CreateDoctor/>}/>
+          <Route path='/editDoctor/:id' element={ <EditDoctor/>}/>
+
+          <Route path='/showAppointment' element={ <ShowAppointment/>} />
+          <Route path='/createAppointment' element={ <CreateAppointment/>}/>
+          <Route path='/editAppointment/:id' element={ <EditAppointment/>}/>
+
+          <Route path='/showAppointment' element={ <ShowHours/>} />
+          <Route path='/createAppointment' element={ <CreateHour/>}/>
+          <Route path='/editAppointment/:id' element={ <EditHour/>}/>
         </Routes>
       </BrowserRouter>
     </div>
